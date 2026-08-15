e9d3cedc9d77c07209b4923e36eff1736636fa94
AvaTar-ArTs <me@avatararts.org>
2026-08-15 19:40:26 -0400

    Export storyworld validation API

--- src/chotaku/__init__.py
+++ src/chotaku/__init__.py
@@ -2,6 +2,7 @@
 
 from .compiler import compile_storyworld
 from .models import StoryWorld
+from .validation import Finding, validate_storyworld
 
-__all__ = ["StoryWorld", "compile_storyworld"]
+__all__ = ["Finding", "StoryWorld", "compile_storyworld", "validate_storyworld"]
 __version__ = "0.1.0"
